17440e7d3ed40594defb53f2a48f586d33b432e2
Srithar810 <srithar.s810@gmail.com>
2025-08-05 16:10:33 +0530

    Login + axios +cors

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,13 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer footer-horizontal footer-center bg-base-300 text-base-content rounded p-10 fixed bottom-0">
-      <nav className="grid grid-flow-col gap-4">
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
+    <footer className="footer footer-horizontal footer-center bg-base-300 text-base-content rounded p-5 fixed bottom-0">
+    
       <nav>
         <div className="grid grid-flow-col gap-4">
           <a>
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import axios from "axios";
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [emailId, setEmailId] = useState("test01@gmail.com");
+  const [password, setPassword] = useState("Test@123");
 
-export default Login
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className=" flex justify-center my-10">
+      <div className="card   bg-base-300  w-96">
+        <div className="card-body">
+          <h2 className="card-title">Login </h2>
+          <div>
+            npm install axios
+            <fieldset className="fieldset ">
+              <legend className="fieldset-legend">Email ID {emailId}</legend>
+              <input
+                type="text"
+                value={emailId}
+                className="input"
+                placeholder="Enter Your Email ID"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="text"
+                value={password}
+                className="input"
+                placeholder="Enter Your Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
