1feb4da38a2af8b430385eaea7699163fa5ddca1
Srithar810 <srithar.s810@gmail.com>
2025-08-06 11:19:32 +0530

    setup redux store

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,31 @@
 import axios from "axios";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("test01@gmail.com");
   const [password, setPassword] = useState("Test@123");
+  const dispatch = useDispatch();
+  const navigate =useNavigate();
 
   const handleLogin = async () => {
+
     try {
       const res = await axios.post(
-        "http://localhost:5000/login",
+        BASE_URL+"/login",
         {
           emailId,
           password,
         },
         { withCredentials: true }
       );
+      // console.log(res.data);
+      dispatch(addUser(res.data));
+      navigate("/feed")
+     
     } catch (err) {
       console.error(err);
     }
@@ -26,9 +37,9 @@ const Login = () => {
         <div className="card-body">
           <h2 className="card-title">Login </h2>
           <div>
-            npm install axios
+            
             <fieldset className="fieldset ">
-              <legend className="fieldset-legend">Email ID {emailId}</legend>
+              <legend className="fieldset-legend">Email ID </legend>
               <input
                 type="text"
                 value={emailId}
